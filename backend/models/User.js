58671c6da986156309457fb456221da8b348d0f3
Beesigaa <adejohadamah@gmail.com>
2024-07-17 10:35:59 -0700

    adding timestamp

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    email: {type: String, required: true, unique: true, match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/}
+    email: {type: String, required: true, unique: true, match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/},
+    roles: [{ type: String }],
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('User', userSchema);
